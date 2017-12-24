7a4abef9d4e6581b8bfe937f5a560774a092e928
longdreamer <lungfei@sina.cn>
2017-12-24 16:17:12 +0800

    add driver for general use of GPIO config

---
+++ Source/driver/drv_GPIO_config/drv_gpio_config.c
@@ -0,0 +1,114 @@
+/**
+  ******************************************************************************
+  * @file    drv_gpio_config.c
+  * @author  fei long
+  * @version V1.0
+  * @date    2017-12-24
+  * @brief   
+  *****************************************************************************
+  */
+#include "drv_gpio_config.h"
+	
+
+/*related port with RCC, used to get RCC from GPIO*/
+typedef struct
+{
+    GPIO_TypeDef* port;
+    T_U32 RCC_Periph;
+}TS_GPIO_PORT_AND_RCC;
+
+/*GPIO Port<->RCC Map:used to get RCC from GPIO*/
+const static TS_GPIO_PORT_AND_RCC g_gpio_rcc_map[] = 
+{
+    {GPIOA, RCC_APB2Periph_GPIOA},
+    {GPIOB, RCC_APB2Periph_GPIOB},
+    {GPIOC, RCC_APB2Periph_GPIOC},
+    {GPIOD, RCC_APB2Periph_GPIOD},
+    {GPIOE, RCC_APB2Periph_GPIOE},
+    {GPIOF, RCC_APB2Periph_GPIOF},
+    {GPIOG, RCC_APB2Periph_GPIOG}   
+};
+ /**
+  * @brief  GPIO general init
+  * @param  GPIO_TypeDef: port, select which GPIO prot to be initialize
+  * @param  T_U16       : pin,  select which GPIO Pin to be used
+  * @param  TE_IO_MODE  : mode, select GPIO input/output mode
+  * @param  TE_BOOLEAN  : init_valie,when it is output mode, this is the init value of output. other mode it is not use
+  * @retval None
+  */
+void Drv_GPIO_Init(GPIO_TypeDef* port, T_U16 pin, GPIOMode_TypeDef mode, TE_BOOLEAN init_value)
+{
+		GPIO_InitTypeDef  GPIO_InitStructure;
+    T_U32 rcc_periph = 0;
+    T_U32 i = 0; 
+
+    /*Select the RCC clock according to related GPIO Port*/
+    for(i=0; i<sizeof(g_gpio_rcc_map)/sizeof(g_gpio_rcc_map[0]); i++)
+    {
+        if(port == g_gpio_rcc_map[i].port)
+        {
+            rcc_periph = g_gpio_rcc_map[i].RCC_Periph;
+            break;
+        }
+    }
+
+    /*Enable GPIO clock*/
+    RCC_APB2PeriphClockCmd(rcc_periph, ENABLE);	
+
+    /*init GPIO*/
+    GPIO_InitStructure.GPIO_Pin = pin;
+    GPIO_InitStructure.GPIO_Mode = mode; 
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;//use this speed for common if no special
+    GPIO_Init(port, &GPIO_InitStructure);
+
+    /*set the initial value*/
+    if((mode == GPIO_Mode_Out_OD) || (mode == GPIO_Mode_Out_PP))
+    {
+        if(init_value)
+        {
+            GPIO_SetBits(port, pin);	
+        }
+        else
+        {
+            GPIO_ResetBits(port, pin);	
+        }
+    }
+}
+
+ /**
+  * @brief  GPIO input init
+  * @param  GPIO_TypeDef: port, select which GPIO prot to be initialize
+  * @param  T_U16       : pin,  select which GPIO Pin to be used
+  * @retval None
+  */
+void Drv_GPIO_InputInit(GPIO_TypeDef* port, T_U16 pin)
+{
+		Drv_GPIO_Init(port, pin, GPIO_Mode_IN_FLOATING, E_FALSE);//using floating mode if no special
+}
+
+ /**
+  * @brief  GPIO input init with mode
+  * @param  GPIO_TypeDef: port, select which GPIO prot to be initialize
+  * @param  T_U16       : pin,  select which GPIO Pin to be used
+  * @param  GPIOMode_TypeDef : mode,use which input mode
+  * @retval None
+  */
+void Drv_GPIO_InputModeInit(GPIO_TypeDef* port, T_U16 pin, GPIOMode_TypeDef mode)
+{
+    Drv_GPIO_Init(port, pin, mode, E_FALSE);
+}
+
+ /**
+  * @brief  GPIO output init
+  * @param  GPIO_TypeDef: port, select which GPIO prot to be initialize
+  * @param  T_U16       : pin,  select which GPIO Pin to be used
+  * @param  TE_IO_MODE  : mode, select GPIO input/output mode
+  * @param  TE_BOOLEAN  : init_valie,when it is output mode, this is the init value of output. other mode it is not use
+  * @retval None
+  */
+void Drv_GPIO_OutputInit(GPIO_TypeDef* port, T_U16 pin, TE_BOOLEAN init_value)
+{
+		Drv_GPIO_Init(port, pin, GPIO_Mode_Out_PP, init_value);
+}
+
+/***************************END OF FILE*******************************/
---
+++ Source/driver/drv_GPIO_config/drv_gpio_config.h
@@ -0,0 +1,45 @@
+/**
+  ******************************************************************************
+  * @file    drv_gpio_config.c
+  * @author  fei long
+  * @version V1.0
+  * @date    2017-12-24
+  * @brief   General use functions for GPIO config
+  *****************************************************************************
+  */
+#ifndef __DRV_GPIO_CONFIG_H
+#define __DRV_GPIO_CONFIG_H
+
+#include "stm32f10x.h"
+#include "common_define.h"
+
+/*enum definition:IO MODE*/
+#if 0
+typedef enum
+{
+	E_IO_MODE_AIN, //Analog In/Out Mode
+	E_IO_MODE_IN_FLOATING, //Input and no Pull Mode
+	E_IO_MODE_IPD, //Input and Pull Down Mode
+	E_IO_MODE_IPU, //Input and Pull Up Mode
+	E_IO_MODE_Out_OD, //Output and Open Drain Mode
+	E_IO_MODE_Out_OD_PD, //Output and Open Drain Mode and Pull Down Mode
+	E_IO_MODE_Out_OD_PU, //Output and Open Drain Mode and Pull Up Mode
+	E_IO_MODE_Out_PP, //Output and Push Pull Mode
+	E_IO_MODE_Out_PP_PD, //Output and Push Pull Mode and Pull Down Mode
+	E_IO_MODE_Out_PP_PU, //Output and Push Pull Mode and Pull Up Mode
+	E_IO_MODE_AF_OD, //Alternate function and Open Drain Mode
+	E_IO_MODE_AF_OD_PD, //Alternate function and Open Drain Mode and Pull Down Mode
+	E_IO_MODE_AF_OD_PU, //Alternate function and Open Drain Mode and Pull Up Mode
+	E_IO_MODE_AF_PP, //Alternate function and  Push Pull Mode 
+	E_IO_MODE_AF_PP_PD, //Alternate function and  Push Pull Mode and Pull Down Mode
+	E_IO_MODE_AF_PP_PU //Alternate function and  Push Pull Mode and Pull Up Mode
+}TE_IO_MODE;
+#endif
+
+/*declare exported functions*/
+extern void Drv_GPIO_Init(GPIO_TypeDef* port, T_U16 pin, GPIOMode_TypeDef mode, TE_BOOLEAN init_value);
+extern void Drv_GPIO_InputInit(GPIO_TypeDef* port, T_U16 pin);
+extern void Drv_GPIO_InputModeInit(GPIO_TypeDef* port, T_U16 pin, GPIOMode_TypeDef mode);
+extern void Drv_GPIO_OutputInit(GPIO_TypeDef* port, T_U16 pin, TE_BOOLEAN init_value);
+
+#endif
